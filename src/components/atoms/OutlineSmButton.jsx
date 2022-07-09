80e2c3d7dde4a7bbd476a2bf2d08b76eae6f93d3
Filip Tomeš <tomes@cngroup.dk>
2022-07-09 17:50:22 +0200

    fixed issue with button type beeing submit, removing ingredients functionality

--- src/components/atoms/OutlineSmButton.jsx
+++ src/components/atoms/OutlineSmButton.jsx
@@ -4,6 +4,7 @@ const OutlineSmButton = ({ btnText, onClick, disabled }) => {
       className="border-2 border-slate-700 px-4 py-1 text-slate-700 hover:text-white rounded hover:rounded-xl hover:bg-slate-600 transition-all duration-300 ease-in-out disabled:opacity-25 disabled:cursor-not-allowed"
       onClick={onClick}
       disabled={disabled}
+      type="button"
     >
       {btnText}
     </button>
